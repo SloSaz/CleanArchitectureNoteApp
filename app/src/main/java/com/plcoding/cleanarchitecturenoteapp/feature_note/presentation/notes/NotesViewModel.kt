64a129f7c4865eb1e36ad51e211e4f0f1055febb
Reno <passive@Mac.home>
2026-08-30 12:57:46 +0200

    at 55 minutes

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -0,0 +1,33 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NotesUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NotesViewModel @Inject constructor(
+    private val notesUseCases: NotesUseCases
+) : ViewModel() {
+
+    private val _state = mutableStateOf(NotesState())
+    val state: State<NotesState> = _state
+
+    fun onEvent(event: NotesEvent) {
+        when(event) {
+            is NotesEvent.Order -> {
+
+            }
+            is NotesEvent.DeleteNote -> {
+
+            }
+            is NotesEvent.RestoreNote -> {
+
+            }
+            is NotesEvent.ToggleOrderSection -> {
+
+            }
+        }
+    }
+}
